e5e71b70d8898426c6a11121e58483cd255d7734
Pierre Sauvage <pierrotws@lacsc-1.staff.ece.fr>
2014-05-21 15:16:13 +0200

    new event to handle deletion by event

---
+++ src/dev/emma/petri/control/event/DeletionEvent.java
@@ -0,0 +1,17 @@
+package emma.petri.control.event;
+
+import java.util.EventObject;
+
+public class DeletionEvent extends EventObject{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3661202200093936092L;
+
+	public DeletionEvent(Object source) {
+		super(source);
+		// TODO Auto-generated constructor stub
+	}
+
+}
